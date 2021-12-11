9be92ef225c12fddf5d0822af20f0bb5b6b48ff9
Brian Hart <brian.hart@xylogix.net>
2021-12-11 08:11:16 -0600

    Update IMainWindow.cs
    Add SignedInChanged event handler

--- GitHubManager/IMainWindow.cs
+++ GitHubManager/IMainWindow.cs
@@ -1,4 +1,6 @@
-﻿namespace GitHubManager
+﻿using System;
+
+namespace GitHubManager
 {
     /// <summary>
     /// Defines the publicly-exposed methods and properties of the main application
@@ -7,8 +9,13 @@
     public interface IMainWindow : IForm
     {
         /// <summary>
+        /// Occurs when the value of the <see cref="P:GitHubManager.IMainWindow.IsSignedIn"/> property changes.
+        /// </summary>
+        event EventHandler SignedInChanged;
+
+        /// <summary>
         /// Gets or sets a value indicating whether the user is signed in.
         /// </summary>
-        bool IsSignedIn { get; set; }
+        bool IsSignedIn { get; }
     }
 }
