b9aa3f8ce09de8d6129f62551b4b9d53884898f7
Brian Hart <brian.hart@xylogix.net>
2021-12-15 10:44:55 -0600

    Update ILoginDialogBox.cs
    * Changed the GitHubLoginInfo property to have a type IGitHubLoginInfo
    * Updated XML documentation of the property to correspond to the new return type.

--- GitHubManager/ILoginDialogBox.cs
+++ GitHubManager/ILoginDialogBox.cs
@@ -3,16 +3,16 @@
     public interface ILoginDialogBox : IForm
     {
         /// <summary>
+        /// Gets a reference to an instance of an object that implements the
+        /// <see cref="T:GitHubManager.IGitHubLoginInfo" /> interface that plays the role
+        /// of an object that contains important login information.
+        /// </summary>
+        IGitHubLoginInfo GitHubLoginInfo { get; }
+
+        /// <summary>
         /// Occurs when important login and authorization information is received from
         /// GitHub.
         /// </summary>
         event GitHubLoginInfoReceivedEventHandler GitHubLoginInfoReceived;
-
-        /// <summary>
-        /// Gets a reference to an instance of
-        /// <see cref="T:GitHubManager.GitHubLoginInfo" /> that contains important login
-        /// information.
-        /// </summary>
-        GitHubLoginInfo GitHubLoginInfo { get; }
     }
 }
