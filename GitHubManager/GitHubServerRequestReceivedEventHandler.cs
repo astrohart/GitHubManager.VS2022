84a40fe64c485729cd95756fd43299d39ae2bddd
Brian Hart <brian.hart@xylogix.net>
2022-07-17 20:03:46 -0600

    Remove extra logging

--- GitHubManager/GitHubServerRequestReceivedEventHandler.cs
+++ GitHubManager/GitHubServerRequestReceivedEventHandler.cs
@@ -1,4 +1,4 @@
-﻿namespace GitHubManager
+namespace GitHubManager
 {
     /// <summary>
     /// Represents a handler for a GitHubServerRequestReceived event.
--- GitHubManager/LoginDialogBoxPresenter.cs
+++ GitHubManager/LoginDialogBoxPresenter.cs
@@ -1,4 +1,4 @@
-﻿using Alphaleonis.Win32.Filesystem;
+using Alphaleonis.Win32.Filesystem;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
