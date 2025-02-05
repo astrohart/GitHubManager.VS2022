d40be5d300b9a3e2edbebb9acfd8ec7edb1264f7
Brian Hart <brian.hart@xylogix.net>
2025-02-05 11:42:39 -0700

    Create the 'GHM.Windows.Presenters' module
    Provides objects and functionality to manage the Presenter(s) of the main window(s) of the application.  The Presenter acts as the middleman between the \"view\" i.e., the window, and the \"model\" i.e., the data, handling all the business logic and updating the view based on user interaction and data retrieved from the model; essentially, it translates user actions into operations on the data and updates the view accordingly.

---
+++ GHM.Windows.Presenters.Interfaces/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GHM.Windows.Presenters.Interfaces")]
+[assembly: AssemblyDescription("Provides objects and functionality to manage the Presenter(s) of the main window(s) of the application.  The Presenter acts as the middleman between the \"view\" i.e., the window, and the \"model\" i.e., the data, handling all the business logic and updating the view based on user interaction and data retrieved from the model; essentially, it translates user actions into operations on the data and updates the view accordingly.  This particular library holds the C# interfaces for this component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX GitHub Manager Sample Application")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2021-25 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX GitHub Manager Sample Application.  GitHub Manager Sample Application is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.  Other brand names, brands, logos, service marks, trademarks, and registered trademarks herein belong to their respective owners.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ae0bcf88-69d8-45e1-9cab-2015f452a07d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
