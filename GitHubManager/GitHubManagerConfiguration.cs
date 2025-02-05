61d8aa9bb3d10fd47733e354bb5c7deddbf7e961
Brian Hart <brian.hart@xylogix.net>
2025-02-04 18:52:43 -0700

    Update GitHubManagerConfiguration.cs
    * Modified JSON property names to use snake_case:
      - `clientId` to `client_id`
      - `clientSecret` to `client_secret`
      - `loginOnStartup` to `login_on_startup`
    * Reformatted `Scopes` property getter and setter for consistency

--- GitHubManager/GitHubManagerConfiguration.cs
+++ GitHubManager/GitHubManagerConfiguration.cs
@@ -27,28 +27,44 @@ namespace GitHubManager
         /// Gets or sets a string containing the ID of the particular GitHub app
         /// we want to utilize with this application.
         /// </summary>
-        [JsonProperty("clientId")]
-        public string ClientId { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
+        [JsonProperty("client_id")]
+        public string ClientId
+        {
+            [DebuggerStepThrough] get;
+            [DebuggerStepThrough] set;
+        }
 
         /// <summary>
         /// Gets or sets a string containing the Client Secret of the OAuth App
         /// to bind to this application.
         /// </summary>
-        [JsonProperty("clientSecret")]
-        public string ClientSecret { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
+        [JsonProperty("client_secret")]
+        public string ClientSecret
+        {
+            [DebuggerStepThrough] get;
+            [DebuggerStepThrough] set;
+        }
 
         /// <summary>
         /// Gets or sets a value that determines whether the application logs in
         /// to GitHub on startup.
         /// </summary>
-        [JsonProperty("loginOnStartup")]
-        public bool LoginOnStartup { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
+        [JsonProperty("login_on_startup")]
+        public bool LoginOnStartup
+        {
+            [DebuggerStepThrough] get;
+            [DebuggerStepThrough] set;
+        }
 
         /// <summary>
         /// Get or sets a collection of strings that defines the scopes to be
         /// used with this application.
         /// </summary>
         [JsonProperty("scopes")]
-        public IList<string> Scopes { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
+        public IList<string> Scopes
+        {
+            [DebuggerStepThrough] get;
+            [DebuggerStepThrough] set;
+        }
     }
 }
