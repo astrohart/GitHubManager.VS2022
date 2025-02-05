9bbc0314911befad0582f49493d290e3b16243c9
Brian Hart <brian.hart@xylogix.net>
2025-02-04 18:48:54 -0700

    Rename 'GitHubManager-Sample-Application' to shorter

--- GitHubManager/Properties/AssemblyInfo.cs
+++ GitHubManager/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GitHubManager-Sample-Application")]
+[assembly: AssemblyTitle("GitHubManager")]
 [assembly: AssemblyDescription("Shows a proof of concept as to how to implement three-legged OAuth to GitHub with an arbitrary GitHub \"app.\"  The Client ID and Client Secret are specified in a config file, so they can be changed to test with any OAuth app you know the client ID and secret of.  This sample demonstrates the techniques associated with how to allow a user to use their GitHub account with your WinForms app.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("xyLOGIX, LLC")]
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-﻿# GitHubManager-Sample-Application `windows application`
+﻿# GitHubManager `windows application`
 
 Shows a proof of concept as to how to implement three-legged OAuth to GitHub with an arbitrary GitHub \"app.\"  The Client ID and Client Secret are specified in a config file, so they can be changed to test with any OAuth app you know the client ID and secret of.  This sample demonstrates the techniques associated with how to allow a user to use their GitHub account with your WinForms app.
