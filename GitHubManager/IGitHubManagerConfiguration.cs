53f3314fb544acfbcdf3fc731e9d02475eb09d15
Brian Hart <brian.hart@xylogix.net>
2025-02-04 18:52:34 -0700

    Update IGitHubManagerConfiguration.cs
    * Refactor properties `ClientId`, `ClientSecret`, `LoginOnStartup`, and `Scopes` to use multi-line definitions.
    * Apply `[DebuggerStepThrough]` attribute individually to getter and setter.
    * Improve readability while maintaining functionality.

--- GitHubManager/IGitHubManagerConfiguration.cs
+++ GitHubManager/IGitHubManagerConfiguration.cs
@@ -13,24 +13,40 @@ namespace GitHubManager
         /// Gets or sets a string containing the ID of the particular GitHub app
         /// we want to utilize with this application.
         /// </summary>
-        string ClientId { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
+        string ClientId
+        {
+            [DebuggerStepThrough] get;
+            [DebuggerStepThrough] set;
+        }
 
         /// <summary>
         /// Gets or sets a string containing the Client Secret of the OAuth App
         /// to bind to this application.
         /// </summary>
-        string ClientSecret { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
+        string ClientSecret
+        {
+            [DebuggerStepThrough] get;
+            [DebuggerStepThrough] set;
+        }
 
         /// <summary>
         /// Gets or sets a value that determines whether the application logs in
         /// to GitHub on startup.
         /// </summary>
-        bool LoginOnStartup { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
+        bool LoginOnStartup
+        {
+            [DebuggerStepThrough] get;
+            [DebuggerStepThrough] set;
+        }
 
         /// <summary>
         /// Get or sets a collection of strings that defines the scopes to be
         /// used with this application.
         /// </summary>
-        IList<string> Scopes { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
+        IList<string> Scopes
+        {
+            [DebuggerStepThrough] get;
+            [DebuggerStepThrough] set;
+        }
     }
 }
