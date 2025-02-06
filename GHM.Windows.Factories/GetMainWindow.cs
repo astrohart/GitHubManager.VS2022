d2b90fa7c53652eaf9219d6e2fb901f6ef4bb33f
Brian Hart <brian.hart@xylogix.net>
2025-02-05 20:03:17 -0700

    Add GetMainWindow.cs and update project references
    * Updated `GHM.Windows.Factories.csproj`:
      - Included new source file `GetMainWindow.cs` for compilation.
      - Added project references: `GHM.Windows.Interfaces` and `GHM.Windows`.
    * Added new file `GetMainWindow.cs`:
      - Defined static class `GetMainWindow` in `GHM.Windows.Factories` namespace.
      - Provided `SoleInstance` method to obtain sole instance of `IMainWindow` from `GitHubManager` namespace.
      - Included logging and exception handling features.

---
+++ GHM.Windows.Factories/GetMainWindow.cs
@@ -0,0 +1,55 @@
+﻿using GitHubManager;
+using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
+using xyLOGIX.Core.Debug;
+
+namespace GHM.Windows.Factories
+{
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see cref="T:GitHubManager.IMainWindow" /> interface.
+    /// </summary>
+    public static class GetMainWindow
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the <see cref="T:GHM.Windows.Factories.GetMainWindow" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetMainWindow() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:GitHubManager.IMainWindow" /> interface, and returns a reference
+        /// to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see cref="T:GitHubManager.IMainWindow" /> interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        public static IMainWindow SoleInstance()
+        {
+            IMainWindow result;
+
+            try
+            {
+                result = MainWindow.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
+    }
+}
