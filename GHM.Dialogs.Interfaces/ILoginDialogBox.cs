8df2e028d6d98193c27415ea7f7d2224dd2de65b
Brian Hart <brian.hart@xylogix.net>
2025-02-06 15:24:50 -0700

    Update multiple files to enhance GitHub login functionality
    * Added using directives for new namespaces:
      - `xyLOGIX.OAuth.GitHub.Events`
      - `xyLOGIX.OAuth.GitHub.Models.Interfaces`
      - `xyLOGIX.UI.Dark.Forms`
    * Added XML documentation comment to `ILoginDialogBox` interface:
      - Describes the interface as a dialog box for GitHub login
    * Updated type reference in `GitHubLoginInfo` property:
      - Changed from `GitHubManager.IGitHubLoginInfo` to `xyLOGIX.OAuth.GitHub.Models.Interfaces.IGitHubLoginInfo`

--- GHM.Dialogs.Interfaces/ILoginDialogBox.cs
+++ GHM.Dialogs.Interfaces/ILoginDialogBox.cs
@@ -1,12 +1,20 @@
 ﻿using System.Diagnostics;
+using xyLOGIX.OAuth.GitHub.Events;
+using xyLOGIX.OAuth.GitHub.Models.Interfaces;
+using xyLOGIX.UI.Dark.Forms;
 
 namespace GHM.Dialogs.Interfaces
 {
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of a dialog box
+    /// that is used to log in to GitHub.
+    /// </summary>
     public interface ILoginDialogBox : IDarkForm
     {
         /// <summary>
         /// Gets a reference to an instance of an object that implements the
-        /// <see cref="T:GitHubManager.IGitHubLoginInfo" /> interface that plays the role
+        /// <see cref="T:xyLOGIX.OAuth.GitHub.Models.Interfaces.IGitHubLoginInfo" />
+        /// interface that plays the role
         /// of an object that contains important login information.
         /// </summary>
         IGitHubLoginInfo GitHubLoginInfo { [DebuggerStepThrough] get; }
