c1a9e27eba6e6aae5256816c26bc5637f2e02627
Brian Hart <brian.hart@xylogix.net>
2025-02-06 15:21:29 -0700

    Update multiple files: improve using directives, docs, and error handling
    * Updated `using` directives to include:
      - `GHM.Config.Providers.Interfaces`
      - `System`
      - `System.Diagnostics`
      - `xyLOGIX.Core.Debug`
    * Updated XML documentation comments to reference:
      - `GHM.Config.Providers.Interfaces.IGitHubManagerConfigProvider` instead of `GitHubManager.IGitHubManagerConfigProvider`
    * Modified `SoleInstance` method to include a try-catch block:
      - Attempts to get the instance of `GitHubManagerConfigProvider`
      - Logs exceptions using `DebugUtils.LogException(ex)`
      - Sets the result to `default` if an exception occurs

--- GHM.Config.Providers.Factories/GetGitHubManagerConfigProvider.cs
+++ GHM.Config.Providers.Factories/GetGitHubManagerConfigProvider.cs
@@ -1,5 +1,8 @@
-﻿using PostSharp.Patterns.Diagnostics;
+﻿using GHM.Config.Providers.Interfaces;
+using PostSharp.Patterns.Diagnostics;
+using System;
 using System.Diagnostics;
+using xyLOGIX.Core.Debug;
 
 namespace GHM.Config.Providers.Factories
 
@@ -7,13 +10,15 @@ namespace GHM.Config.Providers.Factories
     /// <summary>
     /// Provides access to the one and only instance of the object that
     /// implements the
-    /// <see cref="T:GitHubManager.IGitHubManagerConfigProvider" /> interface.
+    /// <see cref="T:GHM.Config.Providers.Interfaces.IGitHubManagerConfigProvider" />
+    /// interface.
     /// </summary>
     public static class GetGitHubManagerConfigProvider
     {
         /// <summary>
         /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:GHM.Config.Providers.Factories.GetGitHubManagerConfigProvider" />
+        /// for the
+        /// <see cref="T:GHM.Config.Providers.Factories.GetGitHubManagerConfigProvider" />
         /// class.
         /// </summary>
         /// <remarks>
@@ -28,17 +33,35 @@ namespace GHM.Config.Providers.Factories
 
         /// <summary>
         /// Obtains access to the sole instance of the object that implements the
-        /// <see cref="T:GitHubManager.IGitHubManagerConfigProvider" /> interface,
+        /// <see cref="T:GHM.Config.Providers.Interfaces.IGitHubManagerConfigProvider" />
+        /// interface,
         /// and returns a reference to it.
         /// </summary>
         /// <returns>
         /// Reference to the one, and only, instance of the object that
         /// implements the
-        /// <see cref="T:GitHubManager.IGitHubManagerConfigProvider" /> interface.
+        /// <see cref="T:GHM.Config.Providers.Interfaces.IGitHubManagerConfigProvider" />
+        /// interface.
         /// </returns>
         [DebuggerStepThrough]
         [return: NotLogged]
         public static IGitHubManagerConfigProvider SoleInstance()
-            => GitHubManagerConfigProvider.Instance;
+        {
+            IGitHubManagerConfigProvider result = default;
+
+            try
+            {
+                result = GitHubManagerConfigProvider.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
     }
 }
