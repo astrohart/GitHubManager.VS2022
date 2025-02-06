9a2f0abeeedaa138f5bd8e4120ff1f1d6ba4e9ad
Brian Hart <brian.hart@xylogix.net>
2025-02-05 20:07:35 -0700

    Add new project references and MakeNewMainWindowPresenter
    * Updated `GHM.Windows.Presenters.Factories.csproj` to include new project references:
      - `GHM.Windows.Interfaces`
      - `GHM.Windows.Presenters.Interfaces`
      - `GHM.Windows.Presenters`
    * Added new static class `MakeNewMainWindowPresenter` in `MakeNewMainWindowPresenter.cs`:
      - Responsible for creating instances of `IMainWindowPresenter`
      - Includes a static constructor and a static method `ForView`
    * Updated `using` directives in `MakeNewMainWindowPresenter.cs` to include:
      - `GHM.Windows.Interfaces`
      - `GHM.Windows.Presenters.Interfaces`
      - `PostSharp.Patterns.Diagnostics`

--- GHM.Windows.Presenters.Factories/MakeNewMainWindowPresenter.cs
+++ GHM.Windows.Presenters.Factories/MakeNewMainWindowPresenter.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using GHM.Windows.Interfaces;
+using GHM.Windows.Presenters.Interfaces;
+using PostSharp.Patterns.Diagnostics;
+
+namespace GHM.Windows.Presenters.Factories
+{
+    /// <summary>
+    /// Creates new instances of objects that implement the
+    /// <see cref="T:GHM.Windows.Presenters.Interfaces.IMainWindowPresenter" />
+    /// interface, and returns references to them.
+    /// </summary>
+    public static class MakeNewMainWindowPresenter
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the
+        /// <see cref="T:GHM.Windows.Presenters.Factories.MakeNewMainWindowPresenter" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static MakeNewMainWindowPresenter() { }
+
+        /// <summary>
+        /// Creates a new instance of an object that implements the
+        /// <see cref="T:GHM.Windows.Presenters.Interfaces.IMainWindowPresenter" />
+        /// interface and returns a reference to it.
+        /// </summary>
+        /// <param name="view">
+        /// (Required.) Reference to an instance of an object that implements the
+        /// <see cref="T:GHM.Windows.Interfaces.IMainWindow" /> interface that represents
+        /// the main application window.
+        /// </param>
+        /// <returns>
+        /// Reference to an instance of an object that implements the
+        /// <see cref="T:GHM.Windows.Presenters.Interfaces.IMainWindowPresenter" />
+        /// interface.
+        /// </returns>
+        public static IMainWindowPresenter ForView(IMainWindow view)
+            => new MainWindowPresenter(view);
+    }
+}
